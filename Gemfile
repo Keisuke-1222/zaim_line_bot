5da84b97b638900569df7567fdc118f1a614a7a2
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2018-11-24 14:50:34 +0900

    Add cooperetion with zaim api

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'line-bot-api'
 gem 'dotenv-rails'
+gem 'oauth'
+gem 'json'
+gem 'http'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,14 +1,9 @@
 class LinebotController < ApplicationController
   require 'line/bot'
+  require 'json'
+  require 'oauth'
 
   protect_from_forgery :except => [:callback]
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-    }
-  end
 
   def callback
     body = request.body.read
@@ -27,7 +22,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: answer_from(event.message['text'])
           }
           client.reply_message(event['replyToken'], message)
         end
@@ -35,5 +30,48 @@ class LinebotController < ApplicationController
     end
 
     head :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    }
+  end
+
+  def total_expense(date)
+    @consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET,
+                                    site: 'https://api.zaim.net',
+                                    request_token_path: '/v2/auth/request',
+                                    authorize_url: 'https://auth.zaim.net/users/auth',
+                                    access_token_path: '/v2/auth/access')
+    @access_token = OAuth::AccessToken.new(@consumer, ACCESS_TOKEN, ACCESS_SECRET)
+
+    params_money = URI.encode_www_form({
+      mode: 'payment',
+      start_date: date,
+      end_date: date,
+    })
+
+    money = @access_token.get("#{API_URL}home/money?#{params_money}")
+    @money = JSON.parse(money.body)
+    @amount_sum = @money_today['money'].inject(0) { |result, n| result + n['amount'] }
+  end
+
+  def answer_from(message)
+    case message
+    when '今日', 'today'
+      date = Date.today.to_s
+    when '昨日', 'yesterday'
+      date = (Date.today - 1).to_s
+    when 'おととい', '一昨日'
+      dete = (Date.today - 2).to_s
+    else
+      return "error"
+    end
+
+    answer = "#{date}の支出は#{total_expense(date)}円です"
   end
 end
